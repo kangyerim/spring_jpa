8aa4c9f9ab2ca0ccd9c3195b1f93401b6f40ef80
kangyerim <yerimkang521@gmail.com>
2020-04-28 12:45:44 +0900

    200428 12:45 first egit

---
+++ yerimspring-1/src/main/java/com/yerimspring/web/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.yerimspring.web;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Yerimspring1Application.class);
+	}
+
+}
---
+++ yerimspring-1/src/test/java/com/yerimspring/web/Yerimspring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yerimspring.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yerimspring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
