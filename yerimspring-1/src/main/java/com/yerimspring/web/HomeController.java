5386056281686ec1f07b106de66c2ca58ae38a8f
kangyerim <yerimkang521@gmail.com>
2020-05-01 15:51:51 +0900

    200501 "15:51"

--- yerimspring-1/src/main/java/com/yerimspring/web/HomeController.java
+++ yerimspring-1/src/main/java/com/yerimspring/web/HomeController.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 	@GetMapping("/")
 	public String hello() {
-		return "index.html";
+		return "index";
+	}
+	
+	@GetMapping("/home")
+	public String home() {
+		return "user";
 	}
 }
